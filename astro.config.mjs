cf042a63ac5cb16f1c7b860dcff617fd08d1e4a5
psp_dada <1824427006@qq.com>
2025-07-23 15:07:02 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,10 +13,12 @@ export default defineConfig({
       },
       // 网站描述
       description: "psp_dada's personal homepage",
-      // 网站图标
+      // 网站图标：在导航栏中设置一个 logo 图片，与网站标题一起显示或替代网站标题
       logo: {
         src: "./public/assets/avatar.jpg",
       },
+      // 设置网站的默认 favicon 的路径
+      favicon: "/assets/avatar.jpg",
       // 为此网站设置英语为默认语言。
       defaultLocale: "root",
       locales: {
@@ -39,10 +41,12 @@ export default defineConfig({
           label: "GitHub",
           href: "https://github.com/pspdada",
         },
+        { icon: "gitlab", label: "GitLab", href: "https://gitlab.com/pspdada" },
       ],
       sidebar: [
         {
           label: "Guides",
+          translations: { "zh-CN": "目录" },
           items: [
             {
               label: "Home",
@@ -50,18 +54,39 @@ export default defineConfig({
               translations: { "zh-CN": "主页" },
             },
             {
+              label: "Blog",
+              translations: { "zh-CN": "随笔" },
+              // 自动生成
+              autogenerate: { directory: "blog" },
+            },
+            {
+              label: "Project",
+              translations: { "zh-CN": "项目" },
+              // 自动生成
+              autogenerate: { directory: "project" },
+            },
+            {
               label: "Research",
               translations: { "zh-CN": "研究" },
-              items: [
-                {
-                  label: "ICCV 2025 | SENTINEL",
-                  link: "/research/sentinel",
-                },
-              ],
+              // 自动生成
+              autogenerate: { directory: "research" },
+              // 手动设置
+              // items: [
+              //   {
+              //     label: "ICCV 2025 | SENTINEL",
+              //     link: "/research/sentinel",
+              //   },
+              // ],
             },
           ],
         },
       ],
+      editLink: {
+        baseUrl: "https://github.com/pspdada/pspdada.github.io/edit/main/",
+      },
+      components: {
+        Footer: "./src/components/Footer.astro",
+      },
     }),
   ],
 });
